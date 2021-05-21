34cd1829ad302a43ce625c19e5704c19722ea3f5
Fatma Kausar <kfatmakausar@gmail.com>
2021-05-20 20:39:40 -0400

    Consoles styling done

--- src/components/Consoles.jsx
+++ src/components/Consoles.jsx
@@ -27,15 +27,36 @@ function Consoles() {
   }
 
   return (
-    <div>
+    <div class="jumbotron mt-5">
+      <h1 class="display-4 text-center">Console Catalog</h1>
+      <p class="lead text-center">
+        Browse throught our catalog and choose your favorite console!
+      </p>
+      <hr class="my-4" />
       {consoles.map((console) => {
         return (
-          <p key={console.id}>
-            <button onClick={() => setSelectedConsole(console.id)}>
-              {console.name}
-            </button>
-            {/* <img src={console.image} alt={console.name}></img> */}
-          </p>
+          <div class="col d-flex justify-content-center">
+            <div class="card w-50 my-3 d-flex d-flex justify-content-center p-2">
+              <p key={console.id}>
+                <img
+                  src={console.image}
+                  class="card-img-top"
+                  alt={console.name}
+                  style={{ height: "250px" }}
+                ></img>
+                <div class="card-body">
+                  <h5 class="card-title text-center">{console.name}</h5>
+                </div>
+                <button
+                  type="button"
+                  class="btn btn-dark btn-lg btn-block"
+                  onClick={() => setSelectedConsole(console.id)}
+                >
+                  Learn More
+                </button>
+              </p>
+            </div>
+          </div>
         );
       })}
     </div>
@@ -43,3 +64,16 @@ function Consoles() {
 }
 
 export default Consoles;
+
+// {consoles.map((console) => {
+//     return (
+//       <p key={console.id}>
+//         <button class = "btn btn-primary" onClick={() => setSelectedConsole(console.id)}>
+//           Learn More
+//         </button>
+//       </p>
+// <img src={console.image} class="card-img-top" alt={console.name}>
+// <div class="card-body">
+// <h5 class="card-title">{console.name}</h5>
+// );
+//   })}
